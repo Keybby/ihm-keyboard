7cd31e4532e5870213512262c155e471063a0a01
rambip <apero1808@gmail.com>
2025-03-31 17:20:49 +0200

    premier prototype avec alpine: keys reactives

---
+++ src/js/code.js
@@ -0,0 +1,27 @@
+class Keyboard {
+  constructor(name) {
+    this.name = name;
+    this.keys = [];
+    this.n_keys = 0;
+  }
+  addKey() {
+    this.keys.push({
+      id: this.n_keys,
+      label: String.fromCharCode(50 + this.n_keys),
+      x: 100 + 100 * this.n_keys,
+      y: 100,
+    });
+    this.n_keys += 1;
+  }
+  clear() {
+    this.keys = [];
+  }
+  getKeys() {
+    return this.keys;
+  }
+}
+class App {
+  constructor() {
+    this.keyboard = new Keyboard();
+  }
+}
