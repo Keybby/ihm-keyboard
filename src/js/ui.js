1da15caadd8daad2c2fb532297bb8d4d5ea55977
stroumpf <nicolas.besson@telecom-paris.fr>
2025-03-31 18:25:35 +0200

    Zoom V2

--- src/js/ui.js
+++ src/js/ui.js
@@ -2,70 +2,29 @@ const view = document.getElementById("main");
 const ui = document.getElementById("ui");
 const body = document.getElementsByTagName("body")[0];
 
-let scale = 1;
-
+let scale = 0.7;
+let x=window.innerWidth/4;
+let y=window.innerHeight/2;
 
 body.addEventListener("mousewheel", function (event) {
+  console.log("scroll detected")
   var deltaY = event.deltaY;
   var deltaX = event.deltaX;
   const threshold = 20;
   if (event.altKey) {
-    // calc nextScale
-    const delta = deltaY || deltaX;
-    const scaleStep =
-      Math.abs(delta) < 50
-        ? 0.05 // touchpad pitch
-        : 0.25; // mouse wheel
-
-    const scaleDelta = deltaY < 0 ? scaleStep : -scaleStep;
-    const nextScale = scale + scaleDelta; // 'scale' is prev scale
-
-    // calc fixedPoint
-    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-    // const fixedPoint = { x: event.clientX, y: event.clientY };
-    const fixedPoint = { x: vw/2, y: vh/2 };
-     
-
-    // scale
-    // 'svgEl' is element to scale
-    for (let i = 0; i < view.children.length; i++) {
-      svgScale(view.children[i], fixedPoint, scale, nextScale);
+    if (Math.abs(deltaY) > threshold) {
+      scale+=Math.sign(deltaY) * 0.1
     }
-    scale = nextScale;
   } else {
     if (Math.abs(deltaY) > threshold) {
-      for (let i = 0; i < view.children.length; i++) {
-        let y = parseInt(view.children[i].getAttribute("y"));
-        y = isNaN(y) ? 0 : y;
-        view.children[i].setAttribute("y", y + Math.sign(deltaY) * 2);
-      }
+      y+=Math.sign(deltaY)*30*scale;
     } else if (Math.abs(deltaX) > threshold) {
-      for (let i = 0; i < view.children.length; i++) {
-        let x = parseInt(view.children[i].getAttribute("x"));
-        x = isNaN(x) ? 0 : x;
-        view.children[i].setAttribute("x", x + Math.sign(deltaX) * 2);
-      }
+      x+=Math.sign(deltaX)*30*scale;
     }
   }
+  view.style.transform=`scale(${scale},${scale})`
+  view.style.transformOrigin=`${x}px ${y}px`
 });
-
-function svgScale(svgEl, fixedPoint, scale, nextScale) {
-  const position = {x:svgEl.getAttribute("x"),y:svgEl.getAttribute("y")}
-  position.x = isNaN(position.x) ? 0 : position.x;
-  position.y = isNaN(position.y) ? 0 : position.y;
-  scalechange = nextScale-scale;
-  svgEl.setAttribute(
-    "x",
-    position.x-(fixedPoint.x*scalechange)
-  
-  );
-  svgEl.setAttribute(
-    "y",
-    position.y-(fixedPoint.y*scalechange)
-  );
-  svgEl.style.transform =  `scale(${nextScale},${nextScale}`
-}
 
 // Drag tool
 function getMousePosition(evt) {
