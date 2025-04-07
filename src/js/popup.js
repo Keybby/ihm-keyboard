8016f6ccf41b98a74a3b32df256d18faa7989be9
rambip <apero1808@gmail.com>
2025-04-07 16:27:42 +0200

    popup when click on key

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,55 +1,67 @@
-class Popup{
-    constructor(){
-        this.html="";
-        this.title="Popup";
-        this.refX=0;
-        this.refY=0;
-        this.x=0;
-        this.y=0;
-        this.offX=0;
-        this.offY=0;
-        this.scale=1;
-        this.dom=document.getElementById("popup_area");
-        this.pop = document.getElementById("popup");
-        this.moving=false;
-    }
+class Popup {
+  constructor() {
+    this.html = "";
+    this.title = "Popup";
+    this.refX = 0;
+    this.refY = 0;
+    this.x = 0;
+    this.y = 0;
+    this.offX = 0;
+    this.offY = 0;
+    this.scale = 1;
+    this.dom = document.getElementById("popup_area");
+    this.pop = document.getElementById("popup");
+    this.moving = false;
+    window.addEventListener("keyup", (event) => {
+      if (event.key === "Escape") {
+        this.close();
+      }
+    });
+  }
 
-    show(){
-        this.dom.style.left="0px";
-        this.dom.style.top="0px";
-        this.x=0;
-        this.y=0;
-        this.offX=0;
-        this.offY=0;
-        this.refX=0;
-        this.refY=0;
-        this.dom.hidden=false;
-    }
+  show() {
+    this.dom.style.left = "0px";
+    this.dom.style.top = "0px";
+    this.x = 0;
+    this.y = 0;
+    this.offX = 0;
+    this.offY = 0;
+    this.refX = 0;
+    this.refY = 0;
+    this.dom.hidden = false;
+  }
 
-    close(event){
-        this.dom.hidden=true;
-        event.stopPropagation();
-    }
+  close() {
+    this.dom.hidden = true;
+  }
 
-    setMoving(event){
-        this.moving=true;
-        this.offX=event.clientX;
-        this.offY=event.clientY;
-    }
+  /**
+   *
+   * @param {MouseEvent} event
+   */
+  setMoving(event) {
+    this.moving = true;
+    this.offX = event.clientX;
+    this.offY = event.clientY;
+  }
 
-    clearMoving(){
-        this.moving=false;
-        this.refX=this.x;
-        this.refY=this.y;
-    }
+  clearMoving() {
+    this.moving = false;
+    this.refX = this.x;
+    this.refY = this.y;
+  }
 
-    move(event){
-        if(this.moving==false) return;
-        this.x=this.refX+(event.clientX-this.offX);
-        this.y=this.refY+(event.clientY-this.offY);
-        this.dom.style.left=`${this.x}px`;
-        this.dom.style.top=`${this.y}px`;
-    }
+  /**
+   *
+   * @param {MouseEvent} event
+   */
+  move(event) {
+    if (this.moving == false) return;
+    this.x = this.refX + (event.clientX - this.offX);
+    this.y = this.refY + (event.clientY - this.offY);
+    this.dom.style.left = `${this.x}px`;
+    this.dom.style.top = `${this.y}px`;
+  }
 }
 
 export default Popup;
