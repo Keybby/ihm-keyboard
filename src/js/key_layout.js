49f0d0773c5bdebde6429a3d3a53f8a2eadbcd21
mleila1312 <leilamelody1904@gmail.com>
2025-04-10 16:01:01 +0200

    commented key_layout.js

--- src/js/key_layout.js
+++ src/js/key_layout.js
@@ -1,8 +1,12 @@
 // import SPECIAL_KEYCODES from "./special_keys.js";
 
 const BEHAVIOUR = {
+  // normal keys : letters, numbers, symbols, ...
   Classic: 0,
+  // keys that are used in combination with others, ex : CTTRL+aLT
   Combo: 1,
+  //keys that need to keep being pressed to have an effect 
+  // on the keyboard, ex : Shift
   Sticky: 2,
 };
 
@@ -16,14 +20,25 @@ class KeyCode {
     this.code = code;
   }
   toString() {
+    // returns the result of the key code from a key
     if (typeof this.code === "number") {
+      // if the keycode is a number, we have a special key such as F12
       return `special key ${this.code}`;
     }
+    // otherwise, the keyboard key is a character
     return this.code.toString();
   }
 }
 
 class KeyLayout {
+
+  /*
+  This class represents the layout of the keys on the keyboard created by the user.
+
+  Attributes : 
+  - behaviour : the behaviour of the key (Classic, Combo, Sticky)
+  - keycodes : the keycodes of the keys on the keyboard
+  */
   /**
    *
    * @param {Number} behaviour
