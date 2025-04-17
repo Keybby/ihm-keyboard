05d43c85d32a1e613e1f0debb211923ac293cfcb
rambip <apero1808@gmail.com>
2025-04-18 00:53:56 +0200

    remove comments in help file

--- src/js/help.js
+++ src/js/help.js
@@ -34,7 +34,7 @@ export const QUESTS = {
 
   RECTANGLE_SELECT: {
     p: 60,
-    text: "With the `move` tool, you can also use rectangle selection.",
+    text: "With the `move` tool, you can also click and drag to create a selection box",
   },
 
   COPY_PASTE: {
@@ -61,23 +61,6 @@ export const QUESTS = {
     p: 1000,
     text: "have fun ! Once your keyboard is created, you can export it.",
   },
-
-  // MULTIPLE_KEYS: {
-  //   p: 50,
-  //   text: "Please select keys, then validate. The keys you select will be the 'activation combo' for this layer.",
-  // },
-
-  // CREATE_LAYER: {
-  //   p: 50,
-  //   text: "Please select keys, then validate. The keys you select will be the 'activation combo' for this layer.",
-  // },
-
-  // // FIXME: this is not a quest
-  // SET_ACTIVATION: {
-  //   p: 50,
-  //   text: "Please select keys, then validate. The keys you select will be the 'activation combo' for this layer.",
-  // },
-  // "Now, the keys you selected are colored in black. The only role of these keys is to switch to the current layer.";
 };
 
 export default class QuestManager {
@@ -126,15 +109,13 @@ export default class QuestManager {
   }
 }
 
-const RANDOM_TIPS = [
-  "To select several keys, you can create a selection box by clicking and dragging your mouse.",
-  "To modify the code of a single key, double click on it.",
-  "To set the key code to a combination of keys, double click on key and toggle the slider!",
-  "To suppress a key, you can use suppr",
-  "To modify the geometry of the key or rotate it, use the geometry panel to the right after selecting the key!",
-  "You can export your creation as an svg or a json by clicking on export.",
-  "If you want to modify previous design that you exported as a json, you can import it by clicking on import.",
-  "If the canvas on which you're drawing the keyboard is too small, you can resize it by dragging the edges of the canvas.",
-  "If you want to move a key, select the move icon at the top center left and drag the key to the desired position.",
-  "If you want to create a key, select the create icon at the top center right and click where you want to place your key.",
-];
+/*
+
+TODO
+
+"To set the key code to a combination of keys, double click on key and toggle the slider!",
+"You can export your creation as an svg or a json by clicking on export.",
+"If you want to modify previous design that you exported as a json, you can import it by clicking on import.",
+"If the canvas on which you're drawing the keyboard is too small, you can resize it by dragging the edges of the canvas.",
+
+*/
